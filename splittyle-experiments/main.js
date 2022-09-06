873ebfeecd0600654e49a57599cb5d9e5c85394c
MrTastix <jksmellie@gmail.com>
2022-09-06 18:36:05 +1200

    Learning SplitText

---
+++ splittyle-experiments/main.js
@@ -0,0 +1,18 @@
+let tl = new TimelineLite(),
+  firstLine = new SplitText(".firstline", { type: "words, lines, chars" });
+  secondLine = new SplitText(".secondline", { type: "words, lines, chars" });
+  dying = new SplitText(".dying", { type: "words, lines, chars", position:"absolute" });
+
+lineOne = firstLine.words;
+lineTwo = secondLine.words;
+dying = dying.chars;
+
+tl.staggerFrom(lineOne, 0.8, { opacity: 0, x: 80, ease: Cubic.easeOut }, 0.05);
+
+tl.staggerFrom(lineTwo, 0.4, { opacity: 0, x: 80, ease: Cubic.easeOut }, 0.1);
+
+tl.staggerFrom(dying, 0.3, { opacity: 0, y: 80, ease: Cubic.easeOut}, 0.1);
+
+document.getElementById("animate").onclick = function () {
+  tl.restart();
+};
