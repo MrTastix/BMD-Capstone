861db84d78f081dfd9675010f37f52297eba8191
MrTastix <jksmellie@gmail.com>
2022-09-07 22:14:12 +1200

    Update splittype.js

--- splittype-experiments/splittype.js
+++ splittype-experiments/splittype.js
@@ -1,42 +1,38 @@
-const timeline = gsap.timeline(),
-    firstLine = new SplitType(".firstLine", { types: 'words, chars', absolute: "true" })
-    secondLine = new SplitType(".secondLine", { absolute: 'true' })
-    dying = new SplitType(".dying", {types: 'chars', absolute: 'true'})
+const tl = gsap.timeline(),
+    firstLine = new SplitType(".firstLine", { types: 'words, chars' })
+    secondLine = new SplitType(".secondLine", { types: 'words, chars' })
+    dying = new SplitType(".dying", {types: 'chars' })
 
 // Animate characters into view with a stagger effect
-gsap.from(firstLine.words, {
+
+
+tl.from(firstLine.words, {
     opacity: 0,
     x: Math.random() * 650 - 100,
-    y: Math.random() * 350 - 100,
+    //y: Math.random() * 350 - 100,
     duration: 0.5,
     stagger: { amount: 0.75 },
-    ease: Quad.easeIn,
-    fontSize: '+=50',
-});
+    ease: Quad.easeIn
+})
 
-gsap.from(secondLine.chars, {
+.from(secondLine.chars, {
     opacity: 0,
-    //: Math.random() * 650 - 100,
-    y: Math.random() * 350 - 100,
+    x: Math.random() * 650 - 100,
+    //y: Math.random() * 350 - 100,
     duration: 0.5,
     stagger: { amount: 0.75 },
-    ease: Quad.easeIn,
-    fontSize: '+=50',
-    delay: 2
-});
+    ease: Quad.easeIn
+})
 
-gsap.from(dying.chars, {
+.from(dying.chars, {
     opacity: 0,
     //x: Math.random() * 650 - 100,
     y: Math.random() * 350 - 100,
     duration: 0.5,
     stagger: { amount: 0.75 },
-    ease: Quad.easeIn,
-    delay: 4
-});
-
+})
 
 
 document.getElementById("reset").onclick = function () {
-    timeline.restart();
+    tl.restart();
 };
