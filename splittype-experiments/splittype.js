c5530c2e5a27622034060155248c2ff3d869d0b2
MrTastix <jksmellie@gmail.com>
2022-09-07 22:22:14 +1200

    SplitType updates
    Making it less overtly offensive for presentations.

--- splittype-experiments/splittype.js
+++ splittype-experiments/splittype.js
@@ -1,17 +1,17 @@
 const tl = gsap.timeline(),
-    firstLine = new SplitType(".firstLine", { types: 'words, chars' })
-    secondLine = new SplitType(".secondLine", { types: 'words, chars' })
-    dying = new SplitType(".dying", {types: 'chars' })
+    firstLine = new SplitType(".firstLine", { types: 'words, chars', absolute: "true" })
+    secondLine = new SplitType(".secondLine", { types: 'words, chars', absolute: "true" })
+    dying = new SplitType(".dying", {types: 'chars', absolute: "true" })
 
 // Animate characters into view with a stagger effect
 
 
-tl.from(firstLine.words, {
+tl.from(firstLine.chars, {
     opacity: 0,
     x: Math.random() * 650 - 100,
     //y: Math.random() * 350 - 100,
-    duration: 0.5,
-    stagger: { amount: 0.75 },
+    duration: 0.25,
+    stagger: { amount: 0.5 },
     ease: Quad.easeIn
 })
 
@@ -26,8 +26,7 @@ tl.from(firstLine.words, {
 
 .from(dying.chars, {
     opacity: 0,
-    //x: Math.random() * 650 - 100,
-    y: Math.random() * 350 - 100,
+    y: -100,
     duration: 0.5,
     stagger: { amount: 0.75 },
 })
